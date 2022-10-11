81bf6c75f8381496224de5038125527aadbd2b15
dingchun <dingdingrao@qq.com>
2022-10-11 22:57:36 +0800

    安装Express框架

--- src/main.js
+++ src/main.js
@@ -1,3 +1,44 @@
-const greeting = 'hello ~';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log(greeting);
+app.listen(port, () => {
+  console.log('🚀 服务已启动');
+});
+
+app.get('/', (request, response) => {
+  response.send('你好');
+});
+
+const data = [
+  {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间',
+  },
+  {
+    id: 2,
+    title: '望月',
+    content: '会当凌绝顶，一览众山小',
+  },
+  {
+    id: 3,
+    title: '忆江南',
+    content: '日出江花红胜火，春来江水绿如蓝',
+  },
+];
+
+app.get('/posts', (request, response) => {
+  response.send(data);
+});
+
+app.get('/posts/:postId', (request, response) => {
+  // 获取数据ID
+  const { postId } = request.params;
+
+  // 查找具体内容
+  const posts = data.filter(item => item.id == postId);
+
+  // 做出响应
+  response.send(posts[0]);
+});
