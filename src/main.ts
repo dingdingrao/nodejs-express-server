147692c20de37e3b79be882dd3cf4ac3a130638d
dingchun <dingdingrao@qq.com>
2022-10-13 15:03:09 +0800

    Node.js 服务端应用开发：设计架构

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import app from './app';
+import { APP_PORT } from './app/app.config';
+
+app.listen(APP_PORT, () => {
+  console.log(`🚀 服务已启动 ~`);
+  console.log(`🚀 http://localhost:${APP_PORT}/`);
+});
