959c60a695b207d6b3c49c81d9ad987901015633
dingchun <dingdingrao@qq.com>
2022-10-16 06:55:06 +0800

    启动服务时增加日志

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,9 @@ import { APP_PORT } from './app/app.config';
 
 app.listen(APP_PORT, () => {
   console.log(`🚀 服务已启动 ~`);
-  console.log(`🚀 http://localhost:${APP_PORT}/`);
+  console.log(`
+  App running at:
+  - Local:   %c http://localhost:${APP_PORT},'color: #43bb88'/
+  - Network: %c http://192.168.10.109:${APP_PORT},'color: #43bb88'/
+  `);
 });
