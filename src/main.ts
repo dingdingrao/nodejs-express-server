28d54e964bdfc89c150a5d6fc689b69c933b87b0
dingchun <dingdingrao@qq.com>
2022-10-21 13:08:22 +0800

    数据连接

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import app from './app';
 import { APP_PORT } from './app/app.config';
+import { connection } from './app/database/mysql';
 
 app.listen(APP_PORT, () => {
   console.log(`🚀 服务已启动 ~`);
   console.log(`
   App running at:
-  - Local:   %c http://localhost:${APP_PORT},'color: #43bb88'/
-  - Network: %c http://192.168.10.109:${APP_PORT},'color: #43bb88'/
+  - Local:   http://localhost:${APP_PORT}
+  - Network: http://192.168.10.109:${APP_PORT}
   `);
+});
+
+/**
+ * 测试使用数据服务连接
+ */
+connection.connect(error => {
+  if (error) {
+    console.log(`👻 连接数据服务失败`, error.message);
+    return;
+  }
+
+  console.log(`😸 成功连接数据服务 ~`);
 });
