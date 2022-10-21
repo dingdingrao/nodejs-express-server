1fb756a8abcd2f49c7c51837175fe9e2260d2081
dingchun <dingdingrao@qq.com>
2022-10-21 16:54:50 +0800

    定义创建用户接口

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,3 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import { UserModel } from './user.model';
 import * as userService from './user.service';
+
+/**
+ * 创建用户
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { name, password } = request.body;
+
+  // 创建用户
+  try {
+    const data = await userService.creatUser({ name, password });
+
+    // 响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
