c7360bb1d79a9340c946a5fd755c9a6771708e0b
dingchun <dingdingrao@qq.com>
2022-11-04 13:53:16 +0800

    定义上传文件接口

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,2 +1,14 @@
 import { Request, Response, NextFunction } from 'express';
 import {} from './file.service';
+
+/**
+ * 上传文件
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  console.log(request.file);
+  response.sendStatus(200);
+};
