cb6d35d8f9d18fa638affa38797cd65454632a11
dingchun <dingdingrao@qq.com>
2022-11-17 09:06:16 +0800

    1、定义保存内容标签服务 2、定义检查内容标签服务

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -76,3 +76,37 @@ export const deletePost = async (postId: number) => {
   // 提供数据
   return data;
 };
+
+/**
+ * 保存内容标签
+ */
+export const creatPostTag = async (postId: number, tagId: number) => {
+  // 准备查询
+  const statement = `
+    INSERT INTO post_tag (postId, tagId)
+    VALUES(?, ?)
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
+
+  // 提供数据
+  return data;
+};
+
+/**
+ * 检查内容标签
+ */
+export const postHasTags = async (postId: number, tagId: number) => {
+  // 准备查询
+  const statement = `
+    SELECT * FROM post_tag
+    WHERE postId = ? AND tagId = ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, [postId, tagId]);
+
+  // 提供数据
+  return (data as Array<any>[0]) ? true : false;
+};
