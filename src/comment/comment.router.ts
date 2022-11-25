5ffa5c48d37f28b2c78d374cdf82bb86f048b1ea
dingchun <dingdingrao@qq.com>
2022-11-25 17:18:19 +0800

    定义发表评论用的接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import * as commentController from './conment.controller';
+import { authGuard } from 'src/auth/auth.middleware';
 
 const router = express.Router();
+
+/**
+ * 发表评论
+ */
+router.post('/comments', authGuard, commentController.store);
 
 /**
  * 导出路由
--- src/comment/conment.controller.ts
+++ src/comment/conment.controller.ts
@@ -1,1 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
+import { creatComment } from './comment.service';
+
+/**
+ * 发表评论
+ */
+export const store = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { id: userId } = request.user;
+  const { content, posetId } = request.body;
+
+  const comment = {
+    content,
+    posetId,
+    userId,
+  };
+
+  try {
+    // 保存数据
+    const data = await creatComment(comment);
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
