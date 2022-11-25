bef06743970fcc5849a5be0b9c55160a374087ee
dingchun <dingdingrao@qq.com>
2022-11-25 17:11:40 +0800

    定义存储评论数据的功能

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,1 +1,19 @@
 import { connection } from '../app/database/mysql';
+import { CommentModel } from './comment.model';
+
+/**
+ * 创建评论
+ */
+export const creatComment = async (comment: CommentModel) => {
+  // 准备数据
+  const statement = `
+    INSERT INTO comment
+    SET ?
+  `;
+
+  // 执行查询
+  const [data] = await connection.promise().query(statement, comment);
+
+  // 提供数据
+  return data;
+};
