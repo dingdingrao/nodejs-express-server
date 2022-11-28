65c0d2c8826ab6e393680beb62e1efa69887a6f3
dingchun <dingdingrao@qq.com>
2022-11-28 11:13:10 +0800

    定义修改评论接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as commentController from './conment.controller';
-import { authGuard } from '../auth/auth.middleware';
+import { accessControl, authGuard } from '../auth/auth.middleware';
 
 const router = express.Router();
 
@@ -13,6 +13,16 @@ router.post('/comments', authGuard, commentController.store);
  * 回复评论
  */
 router.post('/comments/:commentId/reply', authGuard, commentController.reply);
+
+/**
+ * 修改评论
+ */
+router.patch(
+  '/comments/:commentId',
+  authGuard,
+  accessControl,
+  commentController.update,
+);
 
 /**
  * 导出路由
