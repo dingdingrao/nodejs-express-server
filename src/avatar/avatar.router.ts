6858501eb4ad23d48e54e2c97b14a167fc13f37f
dingchun <dingdingrao@qq.com>
2023-01-20 10:56:59 +0800

    创建头像模块<avatar>

---
+++ src/avatar/avatar.router.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as avatarController from './avatar.controller';
+
+const router = express.Router();
+
+/**
+ * 新建路由
+ */
+
+/**
+ * 导出路由
+ */
+export default router;
---
+++ src/avatar/avatat.service.ts
@@ -0,0 +1,1 @@
+import { connection } from 'src/app/database/mysql';
