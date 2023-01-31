0ffc4745e72468311a6071851fbb363305512d71
dingchun <dingdingrao@qq.com>
2023-01-31 10:16:24 +0800

    定义上传头像接口

--- src/avatar/avatar.router.ts
+++ src/avatar/avatar.router.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import * as avatarController from './avatar.controller';
+import { authGuard } from '../auth/auth.middleware';
+import { avatarInterceptor } from './avatar.middleware';
 
 const router = express.Router();
 
 /**
- * 新建路由
+ * 上传头像
  */
+router.post('/avatar', authGuard, avatarInterceptor, avatarController.store);
 
 /**
  * 导出路由
