663afd75b1857f85057956c7fccdef6d65909efd
dingchun <dingdingrao@qq.com>
2023-02-01 17:01:01 +0800

    创建用户点赞内容的数据表

---
+++ src/like/创建用户点赞内容的数据表.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `user_like_post` (
+  `userId` INT(11) NOT NULL,
+  `postId` INT(11) NOT NULL,
+
+  PRIMARY KEY (`userId`, `postId`),
+
+  FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION,
+  FOREIGN KEY (`postId`) REFERENCES `post` (`id`)
+    ON DELETE NO ACTION ON UPDATE NO ACTION
+) DEFAULT CHARSET=utf8mb4
+  COLLATE=utf8mb4_unicode_ci;
