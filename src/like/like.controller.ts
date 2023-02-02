938f922e33ad3deac537067ad59df8f5df004824
dingchun <dingdingrao@qq.com>
2023-02-02 08:47:43 +0800

    1、定义保存点赞内容的功能 2、定义点赞内容接口

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -1,1 +1,26 @@
 import { Request, Response, NextFunction } from 'express';
+import { creatUserLikePost } from './like.service';
+
+/**
+ * 点赞内容
+ */
+export const storeUserLikePost = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  // 准备数据
+  const { postId } = request.params;
+  const { id: userId } = request.user;
+
+  // 点赞内容
+  try {
+    // 执行查询
+    const data = await creatUserLikePost(Number(userId), Number(postId));
+
+    // 做出响应
+    response.status(201).send(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/like/like.router.ts
+++ src/like/like.router.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import { authGuard } from '../auth/auth.middleware';
 import * as likeController from './like.controller';
 
 const router = express.Router();
+
+/**
+ * 点赞内容
+ */
+
+router.post('/post/:postId/like', authGuard, likeController.storeUserLikePost);
 
 /**
  * 导出路由
