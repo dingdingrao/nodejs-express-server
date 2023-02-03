ebdca1f6544fcea3351191545cce6aca97db2036
dingchun <dingdingrao@qq.com>
2023-02-03 15:10:15 +0800

    获取内容的评论列表

--- src/comment/comment.middleware.ts
+++ src/comment/comment.middleware.ts
@@ -8,11 +8,23 @@ export const filter = async (
   response: Response,
   next: NextFunction,
 ) => {
+  // 解构查询符
+  const { post, user, action } = request.query;
+
   // 默认过滤
   request.filter = {
     name: 'default',
     sql: 'comment.parentId IS NULL',
   };
+
+  // 内容的评论
+  if (post && !user && !action) {
+    request.filter = {
+      name: 'postComments',
+      sql: 'comment.parentId IS NULL AND comment.postId = ?',
+      param: post as string,
+    };
+  }
 
   // 下一步
   next();
