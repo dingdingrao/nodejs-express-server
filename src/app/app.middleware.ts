f82d091266d6a2ffc9354a9f79a280a4ddc4d2f6
dingchun <dingdingrao@qq.com>
2023-02-07 08:43:04 +0800

    定义用户帐户接口

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -95,6 +95,11 @@ export const defaultErrorHandler = (
       message = '没找到 ~~ 😵‍💫';
       break;
 
+    case 'USER_NOT_FOUND':
+      statusCode = 404;
+      message = '没找到这个用户 ~~ 😵‍💫';
+      break;
+
     default:
       statusCode = 500;
       message = '服务暂时出了点问题 ⛔';
